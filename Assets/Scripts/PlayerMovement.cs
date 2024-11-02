0ea55bd719f1df5118784149ed56d0679700a214
Ishaan Seth <ishaanseth2005@gmail.com>
2024-11-02 16:52:41 +0530

    adding camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,18 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed = 5;
     public Rigidbody rb;
+    float horizontalInput;
 
     private void FixedUpdate()
     {
         Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
-        rb.MovePosition(rb.position + forwardMove);
+        Vector3 horizontalMove = transform.right * horizontalInput * 2 * speed * Time.fixedDeltaTime; 
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
     }
 }
